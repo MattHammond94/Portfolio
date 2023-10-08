458dc5ece232ed271128e2278ef8a253674a94e7
Matt Hammond <mjhammond94@gmail.com>
2023-10-08 23:03:22 +0100

    Add elements to notfound page comp

--- portfolio/src/Pages/NotFound.jsx
+++ portfolio/src/Pages/NotFound.jsx
@@ -1,7 +1,11 @@
 
 const NotFound = () => {
   return (
-    <div>Nope</div>
+    <div className="not-found-container">
+      <h1 className="not-found-header">404 - Nothing exists here.</h1>
+      <img className="error-icon" src="/error.png" />
+      <a className="not-found-anchor" href="/c">Click here to return home</a>
+    </div>
   )
 }
 
