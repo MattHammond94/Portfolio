3d082ee0328f3542d891440a3ae3d22679feb2a5
Matt Hammond <mjhammond94@gmail.com>
2023-10-08 22:15:06 +0100

    Update elements - Add styling and media queries

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -5,10 +5,12 @@ function App() {
    <div className="container-1">
     <h1>Matt Hammond</h1>
     <h2>Junior Full-Stack Engineer</h2>
-    <img src="/me.png" height={500} width={500}  />
-    <p>I'm a recent graduate of the Makers Academy bootcamp and currently looking for a junior/entry level role.<br />
-    You can contact me through LinkedIn <a href="https://www.linkedin.com/in/matt-hammond-44444444f/">here.</a><br />
-    Or visit my GitHub <a href="https://github.com/MattHammond94">here.</a></p>
+    <img src="/me.png" />
+    
+    <p>Makers Academy bootcamp graduate currently looking for a junior role.</p>
+    <p>To view all my recent projects click <a href="">here</a></p>
+    <p>You can contact me through LinkedIn <a href="https://www.linkedin.com/in/matt-hammond-44444444f/">here</a></p>
+    <p>Or visit my GitHub <a href="https://github.com/MattHammond94">here</a></p>
    </div>
   )
 }
