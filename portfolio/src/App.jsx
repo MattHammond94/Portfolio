45a445c30d610dcdb85ffab7199b1d72b52bc1db
Matt Hammond <mjhammond94@gmail.com>
2023-10-08 11:05:26 +0100

    Add files to public folder - Add all elements to app comp

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -2,8 +2,13 @@
 function App() {
 
   return (
-   <div className="container">
-    <div>Matt Hammond</div>
+   <div className="container-1">
+    <h1>Matt Hammond</h1>
+    <h2>Junior Full-Stack Engineer</h2>
+    <img src="/me.png" height={500} width={500}  />
+    <p>I'm a recent graduate of the Makers Academy bootcamp and currently looking for a junior/entry level role.<br />
+    You can contact me through LinkedIn <a href="https://www.linkedin.com/in/matt-hammond-44444444f/">here.</a><br />
+    Or visit my GitHub <a href="https://github.com/MattHammond94">here.</a></p>
    </div>
   )
 }
