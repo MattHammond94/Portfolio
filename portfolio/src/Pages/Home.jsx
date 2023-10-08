794b81c279052f8145d98f294cf69b10ba631263
Matt Hammond <mjhammond94@gmail.com>
2023-10-08 22:26:51 +0100

    Move home elements into homepage comp - ad routing to app comp

---
+++ portfolio/src/Pages/Home.jsx
@@ -0,0 +1,17 @@
+const Home = () => {
+
+  return (
+    <div className="container-1">
+      <h1>Matt Hammond</h1>
+      <h2>Junior Full-Stack Engineer</h2>
+      <img src="/me.png" />
+      
+      <p>Makers Academy bootcamp graduate currently looking for a junior role.</p>
+      <p>To view all my recent projects click <a href="">here</a></p>
+      <p>You can contact me through LinkedIn <a href="https://www.linkedin.com/in/matt-hammond-44444444f/">here</a></p>
+      <p>Or visit my GitHub <a href="https://github.com/MattHammond94">here</a></p>
+    </div>
+  )
+}
+
+export default Home
