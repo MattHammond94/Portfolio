f188e461a23b7217739d43b5137a3977f81fd250
Matt Hammond <mjhammond94@gmail.com>
2023-10-17 16:18:01 +0100

    Update homepage

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -5,8 +5,7 @@ const Home = () => {
       <h1 className="home-title">Matt Hammond</h1>
       <h2 className="home-sub">Junior Full-Stack Engineer</h2>
       <img className="home-img" src="/me.png" />
-      
-      <p>Makers Academy bootcamp graduate currently looking for a junior role.</p>
+      <p className="first-p">Makers Academy bootcamp graduate currently looking for a junior role.</p>
       <p>To view all my recent projects click <a href="/projects">here</a></p>
       <p>You can contact me through LinkedIn <a href="https://www.linkedin.com/in/matt-hammond-44444444f/">here</a></p>
       <p>Or visit my GitHub <a href="https://github.com/MattHammond94">here</a></p>
--- portfolio/src/Pages/Projects.jsx
+++ portfolio/src/Pages/Projects.jsx
@@ -4,7 +4,7 @@ const Projects = () => {
   const projectInfo = [
     { title: "Phobos", description: "An educational space themed web app built using React.", img: "/phobos.png", link: "" },
     { title: "Java-Gram", description: "An instagram clone built using the MERN stack.", img: "/Insta.jpg", link: "" },
-    { title: "Fours Garden", description: "A portfolio style website to showcase an artists work - Built using React.", img: "/fours-garden.jpg", link: "" },
+    { title: "Fours Garden", description: "A portfolio website to showcase an artists work - Built using React.", img: "/fours-garden.jpg", link: "" },
     { title: "Flock", description: "A multiplatform mobile app built using React Native.", img: "/flock.png", link: "https://github.com/ev-th/Flock" },
     { title: "The Trelloship Of The String", description: "A LOTR themed facebook clone built using the MERN stack.", img: "/trelloship-landing-page.png", link: "https://github.com/georgebarrett/trelloship_of_the_string/tree/main" }
   ]
