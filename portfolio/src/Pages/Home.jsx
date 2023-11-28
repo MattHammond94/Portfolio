a9745f9c6f37ff57d45af77248307d72f5b2c2b0
Matt Hammond <mjhammond94@gmail.com>
2023-11-28 22:47:27 +0000

    Moderate adjustments to styling

--- portfolio/src/Pages/Home.jsx
+++ portfolio/src/Pages/Home.jsx
@@ -5,7 +5,6 @@ const Home = () => {
       <h1 className="home-title">Matt Hammond</h1>
       <h2 className="home-sub">Junior Full-Stack Engineer</h2>
       <img className="home-img" src="/me.png" alt="A photograph of a very smart gentleman"/>
-      <p className="first-p">Makers Academy bootcamp graduate currently looking for a junior role.</p>
       <a href="/projects">Projects</a>
       <a href="https://www.linkedin.com/in/matt-hammond-44444444f/" target="_blank" rel="noreferrer">LinkedIn</a>
       <a href="https://github.com/MattHammond94" target="_blank" rel="noreferrer">Github</a>
